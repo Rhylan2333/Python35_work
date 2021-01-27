20b5d65975143fc1a293536c5ea2906c5edd1cf5
CaiCai <caucaicai@qq.com>
2021-01-27 14:56:25 +0800

    又要去实验室了……

--- chapter_e_a.py
+++ chapter_e_a.py
@@ -3,16 +3,22 @@ Python 实现isNum函数，参数作为一个字符串，
 如果这个字符串属于整数、浮点数、或复数的表示，
 则返回True，否则返回False。
 """
+msg = input("请输入一个字符串（属于整数、浮点数、或复数）：")
+
+
+
 def isNum(string) :
     try :
         num = eval(string)
         if type(num) == float\
            or type(num) == int\
            or type(num) == complex :
-            print("True")
+            return True
         else :
-            prit("False")
+            return False
     except:
         print("请输入“数”！")  # 防止n="非数字，eval()后报错"
-msg = input("请输入一个字符串（属于整数、浮点数、或复数）")
+
+
+
 isNum(msg)
