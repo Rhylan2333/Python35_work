c6419ec024184001351a837b131290cc5dc82e25
CaiCai <caucaicai@qq.com>
2021-03-05 22:14:38 +0800

    这是不知道为什么的错误，用引索跳过而非replace掉

---
+++ 精品试卷 - 03/综合应用题：论语之二.py
@@ -0,0 +1,27 @@
+# 以下代码为提示框架
+# 请在...处使用一行或多行代码替换
+# 请在______处使用一行代码替换
+#
+# 注意：提示框架代码可以任意修改，以完成程序功能为准
+
+
+fi = open("论语-原文.txt", "r")
+fo = open("论语-提纯原文.txt", "w")
+##IDXs = []
+##for i in range(1,20) :
+##    IDX = "({})".format(i)
+##    IDXs.append(IDX)
+##IDXs = set(IDXs)
+string = fi.read()
+i = 0
+while i < len(string):  # 必须小于，不然超出范围，因为while先做后加，引锁前闭后开
+    if string[i] != "(" :
+        fo.write(string[i])
+        i += 1
+    elif string[i] == "(" :
+        fo.write(string[i+3])  # (X)三个字符
+        i += 4  # x下次从第四个字符判断
+    else :
+        pass
+fi.close()
+fo.close()
