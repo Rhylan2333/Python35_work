f301acc8e04024acde14a00ab5cc58092de4ee24
CaiCai <caucaicai@qq.com>
2021-03-05 17:58:58 +0800

    试卷答案有误

--- 精品试卷 - 02/P301-2.py
+++ 精品试卷 - 02/P301-2.py
@@ -3,7 +3,19 @@
 #
 # 注意：提示框架代码可以任意修改，以完成程序功能为准
 
-...
+csv = open("PY301-SunSign.csv", "r", encoding="utf-8")
 ls = []
-...
-      print("{}({})的生日是{}月{}日至{}月{}日之间".format(______))
+lines = csv.readlines()
+for i in range(len(lines)) :
+    row = lines[i].replace("\n","").split(",")
+    ls.append(row)  # 构建矩阵
+s = input("请输入星座序号(例如,5 10):")
+while s != '' :
+    ls_num = s.split()  #分开月 日
+    for row in ls[1:]:  # “序号”是突破点
+        if ls_num[0] == row[2][:-2] and int(ls_num[1]) >= int(row[2][-2:]) :
+            print("{}({})的生日是{}月{}日至{}月{}日之间".format(row[1], row[4], row[2][:-2], row[2][-2:], row[3][:-2], row[3][-2:]))
+        elif ls_num[0] == row[3][:-2] and int(ls_num[1]) <= int(row[3][-2:]) :
+            print("{}({})的生日是{}月{}日至{}月{}日之间".format(row[1], row[4], row[2][:-2], row[2][-2:], row[3][:-2], row[3][-2:]))
+    s = input("请输入星座序号(例如,5 10):")
+csv.close()
